4016afa95c890850d2b65b4f4450b6f841c05ab3
Rishabh Singh <57044551+gitRishabh-singh@users.noreply.github.com>
2020-06-17 07:38:33 -0700

    updated

---
+++ js/script.js
@@ -0,0 +1,114 @@
+function music00() {
+    var audio = document.getElementById("audio00");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function music01() {
+    var audio = document.getElementById("audio01");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function music02() {
+    var audio = document.getElementById("audio02");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+function music03() {
+    var audio = document.getElementById("audio03");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function music04() {
+    var audio = document.getElementById("audio04");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+function music05() {
+    var audio = document.getElementById("audio05");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function music06() {
+    var audio = document.getElementById("audio06");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+function music07() {
+    var audio = document.getElementById("audio07");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function music08() {
+    var audio = document.getElementById("audio08");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+function music09() {
+    var audio = document.getElementById("audio09");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+function music10() {
+    var audio = document.getElementById("audio10");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function music11() {
+    var audio = document.getElementById("audio11");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
